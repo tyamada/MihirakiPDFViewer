46f1abb4d6e24b51ea5fddd1e97e777787d32c9d
tyamada <takuma.yamada22@gmail.com>
2026-08-21 14:17:14 +0900

    Added tests. Improved error handling. Improved accessibility. Fixed an issue with line break codes. Fixed an issue where images would shrink when the search bar was displayed.

---
+++ MihirakiPDFViewerUITests/MihirakiPDFViewerUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  MihirakiPDFViewerUITestsLaunchTests.swift
+//  MihirakiPDFViewerUITests
+//
+//  Created by 山田 琢磨 on 2026/08/21.
+//
+
+import XCTest
+
+final class MihirakiPDFViewerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
